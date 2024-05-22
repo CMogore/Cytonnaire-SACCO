e81330228666a450e4a3d94b2e478d3758c0cfa9
CMogore <chelsea.mogore@gmail.com>
2024-05-22 17:44:06 +0300

    update home view

--- resources/js/Pages/HomeView.vue
+++ resources/js/Pages/HomeView.vue
@@ -4,9 +4,7 @@
         <h1 class="text-2xl font-bold">HOME</h1>
         <router-link to="/test" class="text-blue-500">Take me to Test page</router-link>
         
-        <button @click.prevent="logout" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Logout
-        </button>
+        
         
     </div>
 </template>
@@ -24,27 +22,7 @@ import axios from 'axios';
 //     }
 // };
 export default {
-    methods: {
-      async logout() {
-            try {
-                const token = localStorage.getItem('auth_token');
-                const response = await axios.post('/api/logout', {}, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                });
-                console.log('yess worked')
-                console.log('Logged out:', response.data);
-                localStorage.removeItem('auth_token');
-                localStorage.removeItem('role_id');
-
-                this.$router.push('/login');
-
-            } catch (error) {
-                console.error('Logout failed:', error.response.data);
-            }
-        },
-        
-    }
+   
+      
 }
 </script>
