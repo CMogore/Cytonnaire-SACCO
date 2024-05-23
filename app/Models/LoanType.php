4458a1640886e2437dcba20a6ca44225ec39369b
CMogore <chelsea.mogore@gmail.com>
2024-05-23 10:32:38 +0300

    admin loantype table

--- app/Models/LoanType.php
+++ app/Models/LoanType.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class LoanType extends Model
 {
@@ -11,4 +13,8 @@ class LoanType extends Model
     protected $fillable = [
         'name', 'interest_rate'
     ];
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
